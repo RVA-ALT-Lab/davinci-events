769cf227463c358379fe847b15a9152da228c3eb
Jeff Everhart <Jeffeverhart383@gmail.com>
2018-01-03 11:16:14 -0500

    update analytics page

--- page-templates/analytics.php
+++ page-templates/analytics.php
@@ -6,12 +6,19 @@
  *
  * @package understrap
  */
-$html = file_get_contents(get_stylesheet_directory() . '/davinci-analytics/dist/index.html');
-$dir = get_stylesheet_directory_uri() . '/davinci-analytics/dist/static';
-$clean = str_replace('/static', $dir, $html);
 
-echo $clean;
+if (current_user_can('list_users')){
+    $html = file_get_contents(get_stylesheet_directory() . '/davinci-analytics/dist/index.html');
+    $dir = get_stylesheet_directory_uri() . '/davinci-analytics/dist/static';
+    $clean = str_replace('/static', $dir, $html);
 
+    echo $clean;
+} else {
+    echo '<div class="alert alert-info" role="alert">
+    <p><strong>Oh snap!</strong> 😉 It looks like you don\'t have a profile yet, you should fill one out now.</p>
+    <a class="btn btn-primary" href="davinci-events/add-profile">Add Profile</a>
+  </div>';
+}
 
 wp_footer();
 
